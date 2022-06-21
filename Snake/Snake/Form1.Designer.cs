0e5c36251afa69aac4ad98c421cdcf19cb2f4f99
Leonid Samoshenko <samoshenkoleonid@yandex.ru>
2022-06-21 14:07:02 +0300

    recreating project

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SettingsButton = new System.Windows.Forms.Button();
+            this.InfoButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.BestScoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(196)))), ((int)(((byte)(77)))));
+            this.StartButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(247)))), ((int)(((byte)(238)))));
+            this.StartButton.Location = new System.Drawing.Point(164, 110);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(375, 104);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            // 
+            // SettingsButton
+            // 
+            this.SettingsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(196)))), ((int)(((byte)(77)))));
+            this.SettingsButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(247)))), ((int)(((byte)(238)))));
+            this.SettingsButton.Location = new System.Drawing.Point(164, 228);
+            this.SettingsButton.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.SettingsButton.Name = "SettingsButton";
+            this.SettingsButton.Size = new System.Drawing.Size(375, 104);
+            this.SettingsButton.TabIndex = 0;
+            this.SettingsButton.Text = "Settings";
+            this.SettingsButton.UseVisualStyleBackColor = false;
+            // 
+            // InfoButton
+            // 
+            this.InfoButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(196)))), ((int)(((byte)(77)))));
+            this.InfoButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(247)))), ((int)(((byte)(238)))));
+            this.InfoButton.Location = new System.Drawing.Point(164, 346);
+            this.InfoButton.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.InfoButton.Name = "InfoButton";
+            this.InfoButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.InfoButton.Size = new System.Drawing.Size(375, 104);
+            this.InfoButton.TabIndex = 0;
+            this.InfoButton.Text = "About us";
+            this.InfoButton.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(149)))), ((int)(((byte)(120)))));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 700);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ScoreLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(102)))), ((int)(((byte)(190)))));
+            this.ScoreLabel.Location = new System.Drawing.Point(0, -1);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(118, 37);
+            this.ScoreLabel.TabIndex = 2;
+            this.ScoreLabel.Text = "Score: 0";
+            // 
+            // BestScoreLabel
+            // 
+            this.BestScoreLabel.AutoSize = true;
+            this.BestScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.BestScoreLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(102)))), ((int)(((byte)(190)))));
+            this.BestScoreLabel.Location = new System.Drawing.Point(0, 36);
+            this.BestScoreLabel.Name = "BestScoreLabel";
+            this.BestScoreLabel.Size = new System.Drawing.Size(180, 37);
+            this.BestScoreLabel.TabIndex = 2;
+            this.BestScoreLabel.Text = "Best Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(247)))), ((int)(((byte)(238)))));
+            this.ClientSize = new System.Drawing.Size(699, 698);
+            this.Controls.Add(this.BestScoreLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.InfoButton);
+            this.Controls.Add(this.SettingsButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Snake game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button StartButton;
+        private Button SettingsButton;
+        private Button InfoButton;
+        private PictureBox pictureBox1;
+        private Label ScoreLabel;
+        private Label BestScoreLabel;
     }
 }
